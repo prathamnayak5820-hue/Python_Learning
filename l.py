bcc9dca9dd0479b6e6a026c632817251b037c0eb
Pratham Nayak <prathamnayak5820@gmail.com>
2026-08-13 23:08:41 +0530

    find pair of sum of given number

--- l.py
+++ l.py
@@ -18,3 +18,18 @@ for name in names:
     print(name)
 
 
+# find pairs whose sum is x
+
+q = [2,7,4,8]
+nz= int(input())
+l= []
+for i in range(len(q)):
+    for j in (i+1,len(q)):
+        if q[i]+q[j]==n:
+            l.extend([i,j])
+
+for i in l:
+    print(i,end=" ")
+
+
+
